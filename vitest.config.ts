d0150241c4d2a6a0472fb5ef5eb5923490391279
jiangxd <jiangxd2016@gmail.com>
2022-07-07 14:10:26 +0800

    chore: merge

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vitest/config'
-import Vue from '@vitejs/plugin-vue'
-import Jsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vitest/config';
+import Vue from '@vitejs/plugin-vue';
+import Jsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
   plugins: [Vue(), Jsx()],
@@ -13,6 +13,6 @@ export default defineConfig({
     },
     globals: true,
     environment: 'happy-dom',
-
+    watch: false
   },
-})
+});
