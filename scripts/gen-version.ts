2de8aba6d66eb91a7dd052cdb0477b30bccb91f4
jiangxd <jiangxd2016@gmail.com>
2022-07-25 16:46:56 +0800

    chore: update gen:version

--- scripts/gen-version.ts
+++ scripts/gen-version.ts
@@ -2,8 +2,9 @@ import fs from 'fs';
 import path from 'path';
 import pkg from '../package.json';
 
-export default function genVersion() {
+(() => {
   const srcPath = path.join(path.resolve(), 'src');
   const version = pkg.version;
   fs.writeFileSync(path.resolve(srcPath, 'version.ts'), `export default '${version}';\n`);
 }
+)();
