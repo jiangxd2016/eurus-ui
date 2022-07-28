b9b9cd4dc26f1557971d911eb1becf3124229ca5
jiangxd <jiangxd2016@gmail.com>
2022-07-28 10:11:56 +0800

    chore: commit lint msg

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -1,29 +1,24 @@
 // Invoked on the commit-msg git hook by yorkie.
 
 import fs from 'fs';
-import chalk from 'chalk';
 const args = process.argv.slice(2);
 const msg = fs.readFileSync(args[0], 'utf-8')
   .trim();
 
-console.log(chalk.green('Verifying commit message...', msg));
+console.log(console.log('Verifying commit message...', msg));
 
 const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/;
 
 if (!commitRE.test(msg)) {
   console.log();
   console.error(
-    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
-      'invalid commit message format.'
-    )}\n\n`
-      + chalk.red(
-        '  Proper commit message format is required for automated changelog generation. Examples:\n\n'
-      )
-      + `    ${chalk.green('feat(compiler): add \'comments\' option')}\n`
-      + `    ${chalk.green(
-        'fix(v-model): handle events on blur (close #28)'
-      )}\n\n`
-      + chalk.red('  See .github/commit-convention.md for more details.\n')
+    ` ERROR invalid commit message format.
+    \n
+    'Proper commit message format is required for automated changelog generation. Examples:\n'
+    'feat(compiler): add \'comments\' option'
+    'fix(v-model): handle events on blur (close #28)'
+     \n
+     See .github/commit-convention.md for more details.\n`
   );
   process.exit(1);
 }
