591cdec0c96cffcfa9eb785095955c4ca96583cd
jiangxd <jiangxd2016@gmail.com>
2022-09-26 17:45:33 +0800

    test: update notify test case

--- src/packages/notify/__tests__/index.spec.ts
+++ src/packages/notify/__tests__/index.spec.ts
@@ -1,11 +1,33 @@
-import { mount } from '@vue/test-utils';
-import ENotification from '..';
+import { nextTick } from 'vue';
+import ENotify from '..';
+import sleep from '@/test-utils/sleep';
 
 describe('ENotification', () => {
 
-  it('ENotification snapshot', () => {
-    const wrapper = mount(ENotification);
-    expect(wrapper.html()).toMatchSnapshot();
-    wrapper.unmount();
+  it('ENotify snapshot', async () => {
+    ENotify.info({ content: 'this is notify', duration: 2000 });
+
+    await nextTick();
+
+    expect(document.body.querySelector<HTMLElement>('.eu-overlay-notify')).toMatchSnapshot();
+  });
+  it('should be render content', async () => {
+
+    ENotify.info({ content: 'this is notify', duration: 2000 });
+
+    await nextTick();
+
+    expect(document.body.querySelector<HTMLElement>('.eu-notify-content')?.innerHTML).toMatchInlineSnapshot('"this is notify"');
+  });
+  it('should be duration', async () => {
+    ENotify.info({ content: 'this is notify', duration: 2000 });
+
+    await nextTick();
+
+    // destroy after 2000ms + 20ms
+    await sleep(2020);
+
+    expect(document.body.querySelector<HTMLElement>('.eu-overlay-notify')).toMatchInlineSnapshot('null');
+
   });
 });
