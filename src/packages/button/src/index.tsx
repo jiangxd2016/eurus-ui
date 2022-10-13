1222e14db97b22f2ecec6fc8e6110cc2cea691f4
jiangxd <jiangxd2016@gmail.com>
2022-10-13 16:16:33 +0800

    fix: fix commit msg verify

--- src/packages/button/src/index.tsx
+++ src/packages/button/src/index.tsx
@@ -77,15 +77,16 @@ export default defineComponent({
       if (props.disabled || props.loading) { return; }
       emit('click', e);
     };
-    const ComponentType = props.native ? 'a' : 'button';
+    const ComponentType = props.native ? 'a' : 'button' ;
     return () => (
       <ComponentType
         class={[
-          `${prefix} ${prefix}--${props.type} bg-${props.type}`,
+          `${prefix} ${prefix}--${props.type} bg-${props.type} ${props.type === 'default' ? 'text-black' : 'text-white'
+          }`,
           classNames,
         ]}
         {...props.native}
-        onClick={handleClick}>
+       onClick={handleClick}>
         {slots?.icons && renderSlot(slots, 'icon')}
         {slots?.default && <span>{slots?.default?.()}</span>}
       </ComponentType>
