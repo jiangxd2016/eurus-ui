6caa39be0a2c71d2fc93cdbeb6497917543505aa
jiangxd <jiangxd2016@gmail.com>
2022-10-14 17:28:32 +0800

    chore: update changelog file

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+---
+title: 更新日志
+spline: explain
+toc: false
+docClass: timeline
+---
+
+
---
+++ scripts/changelog.ts
@@ -0,0 +1,54 @@
+import { execSync } from 'child_process';
+import { writeFileSync, readFileSync, createWriteStream } from 'fs';
+import standardChangelog from 'standard-changelog';
+import { version as _version } from '../package.json';
+
+const VERSION_REG = /\d+\.\d+\.\d+/;
+
+function getLastChangeLogCommit() {
+  const gitCommand = 'git blame CHANGELOG.md';
+  const changeLogCommits = execSync(gitCommand, {
+    cwd: process.cwd(),
+    encoding: 'utf-8',
+  }).split('\n');
+
+  return changeLogCommits.find(cmt => VERSION_REG.test(cmt))?.slice(0, 8) ?? '';
+}
+
+function getGitCommitMap(lastCommit) {
+  const gitCommand = `git log --pretty=format:"%H:%cn" ${lastCommit}..HEAD`;
+  const gitLogMap = execSync(gitCommand, { cwd: process.cwd(), encoding: 'utf-8' }).toString();
+  writeFileSync('.gitlogmap', gitLogMap, 'utf8');
+}
+
+async function updateChangeLog() {
+  console.log(` package.json version: ${_version}\n`);
+
+  const lastCommit = getLastChangeLogCommit();
+  const initialChangelogStr = readFileSync('CHANGELOG.md', 'utf8') || '';
+
+  const pageDataStr = `${initialChangelogStr.match(/---[\S\s]+---/)![0]}\n`;
+  const data = initialChangelogStr.split(/---[\S\s]+---/);
+  data.unshift(pageDataStr);
+
+  new Promise((resolve) => {
+    standardChangelog({}, null, { from: lastCommit, to: 'HEAD' })
+      .on('data', (chunk) => {
+        let changeLogStr = chunk.toString().trim();
+        changeLogStr = changeLogStr.replace(/\(([\d-]+)\)/g, '`$1`');
+        changeLogStr = changeLogStr.replace(/^#\s/g, '## ').trim();
+        data.splice(1, 0, `${changeLogStr}\n`);
+      })
+      .on('end', resolve);
+  }).then(() => {
+    getGitCommitMap(lastCommit);
+
+    const writeStream = createWriteStream('CHANGELOG.md', 'utf8');
+    writeStream.write(data.join('\n'));
+    writeStream.end();
+    console.log('generate changelog done.');
+
+  });
+}
+
+updateChangeLog();
