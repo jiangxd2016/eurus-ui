acf4292898340b7c8eeedc47820ece62938c6b7b
jiangxd <jiangxd2016@gmail.com>
2023-02-06 15:54:56 +0800

    fix(component): [tag] fix tag size

--- src/packages/tag/src/index.tsx
+++ src/packages/tag/src/index.tsx
@@ -1,11 +1,12 @@
-import {defineComponent, PropType, ref} from 'vue';
+import type { PropType } from 'vue';
+import { defineComponent, ref } from 'vue';
 import './style.scss';
-import {Size} from "@/packages/_utils/size";
-import {getPrefixCls} from "@/packages/_utils/global-config";
-import EIcon from "@/packages/icons";
-import size from "@/packages/input/demo/Size.vue";
+import type { Size } from '@/packages/_utils/size';
+import { getPrefixCls } from '@/packages/_utils/global-config';
+import EIcon from '@/packages/icons';
+import size from '@/packages/input/demo/Size.vue';
 
-type tagType = `default` | `primary` | `positive` | `warning` | 'danger' | 'info'
+type tagType = 'default' | 'primary' | 'positive' | 'warning' | 'danger' | 'info';
 const ETagProps = {
   type: {
     type: String as PropType<tagType>,
@@ -15,9 +16,9 @@ const ETagProps = {
     type: Boolean,
     default: false
   },
-  color: {default: ''},
-  borderColor: {default: ''},
-  bgColor: {default: ''},
+  color: { default: '' },
+  borderColor: { default: '' },
+  bgColor: { default: '' },
   size: {
     type: String as PropType<Size>,
     default: 'md',
@@ -28,7 +29,7 @@ export default defineComponent({
   name: 'ETag',
   props: ETagProps,
   emits: ['click', 'close'],
-  setup(props, {slots, emit}) {
+  setup(props, { slots, emit }) {
 
     const prefixCls = getPrefixCls('tag');
     const visible = ref(true);
@@ -55,20 +56,20 @@ export default defineComponent({
           color: props.color,
           borderColor: props.borderColor,
           backgroundColor: props.bgColor,
-          display: visible.value ? 'inline-flex' : "none"
+          display: visible.value ? 'inline-flex' : 'none'
 
         }}
         onClick={tagClick.bind(this)}
       >
         {visible.value}
         {slots?.default && slots.default()}
-        {props.closable &&
-          <span onClick={() => closeClick()} aria-hidden="true" class={`${prefixCls}-icon`}>
-           <EIcon name="close" size={size.value}></EIcon>
+        {props.closable
+          && <span onClick={() => closeClick()} aria-hidden="true" class={`${prefixCls}-icon`}>
+            <EIcon name="close" size={size.value}></EIcon>
           </span>
         }
 
-   </span>
+      </span>
 
     );
   },
