7da718a18aa24e3e9efc3a0e15faf8982c01ffde
jiangxd <jiangxd2016@gmail.com>
2023-02-26 22:19:18 +0800

    test(date-picker): update modelValue

--- src/packages/date-picker/__tests__/index.spec.ts
+++ src/packages/date-picker/__tests__/index.spec.ts
@@ -1,11 +1,26 @@
 import { mount } from '@vue/test-utils';
+import { nextTick } from 'vue';
 import EDatePicker from '..';
+import EuSelectDown from '../../select-down';
 
-describe('EDatePicker', () => {
+describe('EDatePicker ', () => {
 
-  it('EDatePicker snapshot', () => {
-    const wrapper = mount(EDatePicker);
+  it('should work with `modelValue` props', async () => {
+
+    const wrapper = mount(EDatePicker, {
+      props: {
+        modelValue: undefined
+      },
+    });
+    wrapper.setProps({ modelValue: '2023-02-26' });
+    await nextTick();
     expect(wrapper.html()).toMatchSnapshot();
-    wrapper.unmount();
+
+    expect(wrapper.findComponent(EuSelectDown).find('.eu-select-down-single').text()).toBe('2023-02-26');
+
+    wrapper.setProps({ modelValue: '2023-02-27' });
+    await nextTick();
+    expect(wrapper.findComponent(EuSelectDown).find('.eu-select-down-single').text()).toBe('2023-02-27');
   });
+
 });
