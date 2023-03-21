76aa5eb07722aa5f748f247bdd022e128186e76a
jiangxd <jiangxd2016@gmail.com>
2023-03-21 22:15:22 +0800

    chore: update snaphots

--- src/packages/_utils/__tests__/size.spec.ts
+++ src/packages/_utils/__tests__/size.spec.ts
@@ -2,8 +2,8 @@ import { getSize, sizeToPx } from '@/packages/_utils/size';
 
 describe('size', () => {
   it('should work with size`', () => {
-    expect(sizeToPx('md')).toBe(16);
-    expect(getSize('md')).toBe('16px');
+    expect(sizeToPx('md')).toBe(20);
+    expect(getSize('md')).toBe('20px');
     expect(getSize(12)).toBe('12px');
     expect(getSize('12')).toBe('12px');
   });
