8c303d0a579ec947d5187a0a3df3815093672cbc
jiangxd <jiangxd2016@gmail.com>
2023-09-06 16:11:48 +0800

    chore: update deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,27 +5,24 @@ import Jsx from '@vitejs/plugin-vue-jsx';
 
 const dirname = resolve();
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@/': `${resolve(dirname, 'src')}/`,
-    },
-  },
-  define: {
-    __DEV__: true,
-  },
-  plugins: [Vue(), Jsx()],
-  test: {
-    include: ['**/__tests__/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    transformMode: {
-      web: [/\.[jt]sx$/],
-    },
-    coverage: {
-      provider: 'c8',
-      exclude: ['docs/**/*', 'script/**/*'],
-      reporter: ['text', 'json', 'html'],
-    },
-    globals: true,
-    environment: 'happy-dom',
-    watch: false,
-  },
+	resolve: {
+		alias: {
+			'@/': `${resolve(dirname, 'src')}/`,
+		},
+	},
+	define: {
+		__DEV__: true,
+	},
+	plugins: [Vue(), Jsx()],
+	test: {
+		include: ['**/__tests__/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		coverage: {
+			provider: 'v8',
+			exclude: ['docs/**/*', 'script/**/*'],
+			reporter: ['text', 'json', 'html'],
+		},
+		globals: true,
+		environment: 'happy-dom',
+		watch: false,
+	},
 });
